54bf562ce2a1c8b070b5530b1b6ecf36e42c289b
NataliiaKoval <hakunata@gmail.com>
2023-09-17 21:30:31 +0200

    Create Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:git_repos_search/data/entity/git_repo_entity.dart';
 import 'package:git_repos_search/di/providers.dart';
-import 'package:git_repos_search/presentation/search_screen/search_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
+
+import 'presentation/splash_screen/splash_page.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SearchPage(),
+        home: const SplashPage(),
       ),
     );
   }
